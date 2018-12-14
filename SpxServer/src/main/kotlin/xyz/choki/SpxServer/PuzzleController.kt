d1243a77871d55bc4a1f67e926ce423dce71e349
Choki <chokij@outlook.com>
2018-12-14 21:50:32 +0800

    初版服务端

--- SpxServer/src/main/kotlin/xyz/choki/SpxServer/PuzzleController.kt
+++ SpxServer/src/main/kotlin/xyz/choki/SpxServer/PuzzleController.kt
@@ -1,22 +1,21 @@
 package xyz.choki.SpxServer
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.apache.tomcat.jni.SSL.setPassword
-import java.io.FileNotFoundException
-import org.springframework.util.ResourceUtils
-import java.net.URL
+import java.io.InputStreamReader
+import java.io.BufferedReader
+import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.FileSystemResource
+
 
 @RestController
 public class PuzzleController {
     @GetMapping("/puzzles")
     fun getPuzzles(id : Int) : String
     {
-        val file = ResourceUtils.getFile("classpath:puzzles/puzzle$id.json")
-        return file.readText()
+        val resource = FileSystemResource("./puzzles/puzzle$id.json")
+        val br = BufferedReader(InputStreamReader(resource.getInputStream()))
+        return br.readText()
     }
 
 }
